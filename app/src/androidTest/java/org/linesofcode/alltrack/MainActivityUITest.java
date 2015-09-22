4fe2c42f42de7b6a46f69c5dedf79e92cf2abc04
Jonas Jurczok <jonas.jurczok@zalando.de>
2015-09-22 22:15:04 +0200

    added appbar manipulation tests

--- app/src/androidTest/java/org/linesofcode/alltrack/MainActivityUITest.java
+++ app/src/androidTest/java/org/linesofcode/alltrack/MainActivityUITest.java
@@ -1,18 +1,26 @@
 package org.linesofcode.alltrack;
 
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.action.GeneralSwipeAction;
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
+import android.test.ViewAsserts;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.GeneralLocation.BOTTOM_CENTER;
 import static android.support.test.espresso.action.GeneralLocation.CENTER_LEFT;
 import static android.support.test.espresso.action.GeneralLocation.CENTER_RIGHT;
+import static android.support.test.espresso.action.GeneralLocation.TOP_CENTER;
 import static android.support.test.espresso.action.Press.FINGER;
 import static android.support.test.espresso.action.Swipe.FAST;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -21,6 +29,7 @@ import static org.hamcrest.CoreMatchers.not;
 public class MainActivityUITest {
 
     public static final GeneralSwipeAction SWIPE_OPEN = new GeneralSwipeAction(FAST, CENTER_LEFT, CENTER_RIGHT, FINGER);
+    public static final GeneralSwipeAction SWIPE_UP = new GeneralSwipeAction(FAST, BOTTOM_CENTER, TOP_CENTER, FINGER);
 
     @Rule
     @SuppressWarnings("unchecked")
@@ -52,15 +61,29 @@ public class MainActivityUITest {
     }
 
     @Test
-    public void navigationViewShouldBeRendered() throws InterruptedException {
+    public void navigationViewShouldBeRendered() {
         onView(withId(R.id.mainLayout)).perform(SWIPE_OPEN);
         onView(withId(R.id.navigation)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void navigationViewShouldBeClosable() throws InterruptedException {
+    public void navigationViewShouldBeClosable() {
         onView(withId(R.id.mainLayout)).perform(SWIPE_OPEN);
-        onView(withId(R.id.mainLayout)).perform(ViewActions.swipeLeft());
+        onView(withId(R.id.mainLayout)).perform(swipeLeft());
         onView(withId(R.id.navigation)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void swipeUpShouldMinimizeAppBar() {
+        onView(withId(R.id.mainLayout)).perform(swipeUp());
+        onView(withId(R.id.navigation)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void swipeDownShouldMaximizeAppBar() throws InterruptedException {
+        onView(withId(R.id.mainLayout)).perform(swipeUp());
+        onView(withId(R.id.mainLayout)).perform(swipeDown());
+
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
     }
 }
